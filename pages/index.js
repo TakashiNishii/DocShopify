fa5a4676e4b2faf533447dc1cf0df6e42cde7305
Takashi <ygortn14@gmail.com>
2022-01-15 16:10:39 -0400

    :tada: Criação inicial usando o Shopify CLI

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page, TextStyle, Layout, EmptyState  } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Shopify app with Node and React 🎉</Heading>
+  </Page>
+);
+
+export default Index;
